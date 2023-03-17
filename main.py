7db71ddac0ec62cf549d2297e0a5226f51a1a5bf
MVCowley <mvc28@bath.ac.uk>
2023-03-17 07:50:18 +0000

    Added prints for monitoring simulation progress

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ if __name__ == '__main__':
     RESOLUTION = 9
     LEVELS = pd.read_csv('levels.csv')
 
-    devices = param_array(FACTORS, RESOLUTION, LEVELS) # cap on processes
+    devices = param_array(FACTORS, RESOLUTION, LEVELS)
 
     with mp.Pool(processes=PROCESSES) as pool:
         async_results = [pool.apply_async(run_im, (device, PATH)) for device in devices]
@@ -23,10 +23,13 @@ if __name__ == '__main__':
                 py_result = {}
                 for key in result.keys():
                         py_result[key] = [i for i in result[key]._data]
+                print(f'Device {index} solved by IM')
             except TimeoutError:
                 py_result = "timeout"
+                print(f'Device {index} timed out')
             except Exception:
                  py_result = traceback.format_exc()
+                 print(f'Device {index} threw an error, check output for traceback')
 
             with open(f'output/device_{index}.txt', 'w') as result_file:
                     result_file.write(json.dumps(py_result))
